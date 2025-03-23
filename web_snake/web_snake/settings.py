1d03020c88328d32006a89d017eb42c43fda41ac
Daniil Rozhkov <duniilroe@gmail.com>
2025-03-24 00:07:36 +0500

    Configure base.html

--- web_snake/web_snake/settings.py
+++ web_snake/web_snake/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'pages.apps.PagesConfig',
     'rating.apps.RatingConfig',
     'users.apps.UsersConfig',
@@ -124,8 +125,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
